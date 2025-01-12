b489a640bb6dc1ccb04421624385b35b5590cfa1
meekaaku <meekaaku@gmail.com>
2025-01-12 19:04:46 +0000

    fixed urls

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -155,7 +155,7 @@
                 {#each tsResult.hits as hit}
                 <div class="col-12 col-sm-6 col-md-4 col-xl-3 outer">
                     <div class="inner shadow">
-                    <a href={`/view?id=${hit.document.id}`} target="_blank">
+                    <a href={`/gazettey/view?id=${hit.document.id}`} target="_blank">
                     <img src={`https://haley.sgp1.cdn.digitaloceanspaces.com/gazettey/${hit.document.filename.replace('.pdf','_1.jpg')}`} class="img-fluid" alt="Gazzete">
                     </a>
                     </div>
